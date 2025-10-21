f43c01e33312e8f85efb366dc97549dcb6a1f228
Visal1122 <visalyang19@gmail.com>
2025-10-21 21:28:40 +0700

    Add Variable Demo

---
+++ Java Basic/VariableDemo/src/com/visa/javaDemo/VariableDemo.java
@@ -0,0 +1,37 @@
+package com.visa.javaDemo;
+
+public class VariableDemo {
+    public static void main(String[] args){
+//        int age = 18;
+//        int limit = 2;
+//        int a;
+//        a = 10;
+//        a = 25;
+//        a = 32;
+//
+//        int x;
+//        int maxAge;
+//        int limitOfNumber;
+//
+//        boolean isTrue = true;
+//
+//        char letter = 'a';
+//        char letter1 = 62;
+//
+//
+//        System.out.println(a);
+//        System.out.println(isTrue);
+//
+//        System.out.println(letter);
+//        System.out.println(letter1);
+
+        String java = "learn Java";
+
+        int a = 12;
+        int b = 5;
+
+        float c = (float) a /b;
+        System.out.println(c);
+    }
+
+}
